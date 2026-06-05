71cceb27bdb34e914b2d07b1e0df3de971d3d6ae
Devendra Prabhu <devendraprabhu05@gmail.com>
2026-06-05 22:47:35 +0530

    Fixed the dependenyf

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { supabase } from "../supabase"
-
+//geeeee
 
 const Nav = () => {
     const handleLogin = async()=>{
@@ -13,6 +13,7 @@ const Nav = () => {
       
     }
     console.log(supabase.auth.getSession())
+    console.log(supabase.auth.getUser())
   return (
     
     <nav className="max-w-7xl mx-auto px-6 py-5 flex items-center justify-between sticky top-0 z-50 bg-white/70 backdrop-blur-md">
