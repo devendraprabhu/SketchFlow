964034ac8d46a89a34bdbc1bee2cd97f138d4556
Devendra Prabhu <devendraprabhu05@gmail.com>
2026-06-05 23:26:39 +0530

    I hope this works

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,40 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase"
-//geeeee
 
 const Nav = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogin = async()=>{
-        await supabase.auth.signInWithOAuth({
-            provider:"google",
-            options:{
-                redirectTo:`${window.location.origin}/dashboard`
-            }
-        })
-}
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/dashboard`,
+      },
+    });
+  };
 
- const handlesession = async()=>{
-  const { data } = await supabase.auth.getSession()
-    if(data?.session){
-      navigate("/dashboard")
+  const handlesession = async () => {
+    const { data } = await supabase.auth.getSession();
+    if (data?.session) {
+      navigate("/dashboard");
     }
- }
+  };
 
- useEffect(()=>{
-  handlesession()
- },[])
+  useEffect(() => {
+    handlesession();
+  }, []);
 
   return (
-    
     <nav className="max-w-7xl mx-auto px-6 py-5 flex items-center justify-between sticky top-0 z-50 bg-white/70 backdrop-blur-md">
-   
-      <h1 className="text-6xl  tracking-tight">
-        SketchFlow
-      </h1>
+      <h1 className="text-6xl tracking-tight">SketchFlow</h1>
+      
       <ul className="hidden md:flex items-center gap-8 text-sm font-medium text-gray-600">
-        <li className="hover:text-black cursor-pointer text-medium">Home</li>
+        <li className="hover:text-black cursor-pointer font-medium">Home</li>
         <li className="hover:text-black cursor-pointer">Pricing</li>
         <li className="hover:text-black cursor-pointer">About me</li>
       </ul>
+      
       <div className="flex items-center gap-4">
         <button onClick={handleLogin} className="text-sm font-medium text-gray-600 hover:text-black cursor-pointer">
           Login
